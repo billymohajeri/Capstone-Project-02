9b8b809c39df412696cbb70deb9256f201777e83
elarhadu <elarhadu@gmail.com>
2023-01-03 16:45:36 +0000

    Fetch data from api and render it dynamically

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
 import './index.css';
+import displayShows from './modules/display';
+import getShows from './modules/getShows';
+import { API_URL } from './modules/utils';
 
+getShows();
+displayShows();
--- src/modules/getShows.js
+++ src/modules/getShows.js
@@ -1,13 +1,9 @@
 import { API_URL } from './utils.js';
 
-
-getShows()
-
 const getShows = async () => {
   const response = await fetch(`${API_URL}/?q=starwars`);
-    const data = await response.json();
-    console.log(data);
-    // return data;
+  const data = await response.json();
+  return data;
 };
 
 export default getShows;
