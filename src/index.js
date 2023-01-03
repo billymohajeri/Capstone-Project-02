31f289af1cbd487238b47f1f9a17460b8042619a
elarhadu <elarhadu@gmail.com>
2023-01-03 16:47:49 +0000

    Fetch data from api and render it dynamically

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './index.css';
 import displayShows from './modules/display';
 import getShows from './modules/getShows';
-import { API_URL } from './modules/utils';
 
 getShows();
 displayShows();
