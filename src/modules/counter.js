a1937d4f7a13ed0162ba9ce87cc8c56a50114411
elarhadu <elarhadu@gmail.com>
2023-01-04 10:56:49 +0000

    Create a function to display the number of cards showing on the page

---
+++ src/modules/counter.js
@@ -0,0 +1,15 @@
+class Movies {
+  constructor(number) {
+    this.number = number;
+  }
+
+  getNumber() {
+    return this.number;
+  }
+
+  setNumber(index) {
+    this.number = index;
+  }
+}
+
+export default Movies;
