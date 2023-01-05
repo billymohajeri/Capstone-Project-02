05aea218e3349a045e904615b941e61281abeddb
Billy Mohajeri <billy.mohajeri@gmail.com>
2023-01-05 17:53:13 +0200

    Add comments counter

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import './index.css';
 import displayShows from './modules/display.js';
 import getShows from './modules/getShows.js';
 import displayComments from './modules/displayComments.js';
+import displayLikes from './modules/likes.js';
 
 getShows();
 displayShows();
 displayComments();
+displayLikes();
--- src/modules/displayComments.js
+++ src/modules/displayComments.js
@@ -1,6 +1,9 @@
 import { BASE_URL } from './utils.js';
+import commentCounter from './commentCounter.js';
 
 const commentPopup = document.querySelector('.comment-popup');
+const popup = document.querySelector('.popup');
+
 const getComments = async (i) => {
   const response = await fetch(`${BASE_URL}/comments?item_id=item${i}`);
   const data = await response.json();
@@ -8,22 +11,28 @@ const getComments = async (i) => {
 };
 
 const displayComments = async () => {
+  const popupSection = document.querySelector('.popup');
+  if (!popupSection.getElementsByTagName('h2').length) {
+    popupSection.innerHTML += '<i class="fa-solid fa-xmark fa-xl"></i>';
+    const h2 = document.createElement('h2');
+    h2.id = 'comments';
+    popupSection.prepend(h2);
+  }
+
   document.addEventListener('click', async (event) => {
     if (event.target.classList.contains('fa-xmark')) {
-      commentPopup.style.display = 'none';
+      popup.style.display = 'none';
     }
     if (event.target.classList.contains('fa-comment')) {
-      commentPopup.style.display = 'block';
       const dataComments = await getComments(event.target.id);
-      const h2 = document.createElement('h2');
-      commentPopup.innerHTML = '<i class="fa-solid fa-xmark fa-xl"></i>';
-      h2.innerText = `Comments (${dataComments.length})`;
-      commentPopup.append(h2);
       let commentText = '';
+      commentPopup.innerText = '';
       for (let index = 0; index < dataComments.length; index += 1) {
         commentText = `${dataComments[index].creation_date} ${dataComments[index].username}: ${dataComments[index].comment}`;
         commentPopup.innerHTML += `<p>${commentText}</p>`;
+        popup.style.display = 'block';
       }
+      commentCounter();
     }
   });
 };
