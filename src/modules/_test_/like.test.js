d77b4660b3f80b22ecb0e027f94d447cebd066ef
elarhadu <elarhadu@gmail.com>
2023-01-06 19:01:49 +0000

    Create a jest unit test for the like api

---
+++ src/modules/_test_/like.test.js
@@ -0,0 +1,45 @@
+import { getLikes, addLike } from '../likes.js';
+import { BASE_URL } from '../utils.js';
+
+describe('getLikes', () => {
+  it('should return data from the /likes endpoint', async () => {
+    // Arrange
+    const mockSuccessResponse = {};
+    const mockJsonPromise = Promise.resolve(mockSuccessResponse);
+    const mockFetchPromise = Promise.resolve({
+      json: () => mockJsonPromise,
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    // Act
+    const result = await getLikes();
+
+    // Assert
+    expect(result).toEqual(mockSuccessResponse);
+  });
+});
+
+describe('addLike', () => {
+  it('should send a POST request to the /likes endpoint', async () => {
+    // Arrange
+    const mockSuccessResponse = 'Success';
+    const mockFetchPromise = Promise.resolve({
+      text: () => mockSuccessResponse,
+    });
+    jest.spyOn(global, 'fetch').mockImplementation(() => mockFetchPromise);
+
+    // Act
+    const result = await addLike('123');
+
+    // Assert
+    expect(result).toEqual(mockSuccessResponse);
+    expect(fetch).toHaveBeenCalledWith(
+      `${BASE_URL}/likes`,
+      expect.objectContaining({
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ item_id: '123' }),
+      }),
+    );
+  });
+});
